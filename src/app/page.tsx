8ea73aee2596396e41dd4a24b55c5c6e37bc5797
Abhishek Rajput <abhishekrajput9111606916@gmail.com>
2024-10-06 12:20:01 +0530

    added local code to git

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { Header } from "@/sections/Header";
+import { HeroSection } from "@/sections/Hero";
+import { ProjectsSection } from "@/sections/Projects";
+import { TapeSection } from "@/sections/Tape";
+import { TestimonialsSection } from "@/sections/Testimonials";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Header />
+      <HeroSection />
+      <ProjectsSection />
+      <TapeSection/>
+      <TestimonialsSection/>
+    </div>
+  );
+}
